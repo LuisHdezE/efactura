c73c2e75151ae0be627a894ecc4d270707c4ab08
MSc Luis A. Hernández Elias <88979457+LuisHdezE@users.noreply.github.com>
2026-08-30 02:05:24 -0300

    refactor(deps): preserve legacy generic mapper overload

--- src/ApplicationCore/Mapping/ILegacyObjectMapper.cs
+++ src/ApplicationCore/Mapping/ILegacyObjectMapper.cs
@@ -7,5 +7,6 @@ namespace AutoMapper
     public interface IMapper
     {
         TDestination Map<TDestination>(object source);
+        TDestination Map<TSource, TDestination>(TSource source);
     }
 }
